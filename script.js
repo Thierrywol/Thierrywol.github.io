8f08c4fecffe2589342884604b5edc8e8ebb2e2c
Thierrywol <tdwolffenbuttel1@gmail.com>
2025-06-01 12:34:32 +0200

    werkende volgende knoppen

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const pre = document.getElementById("pre-questionnaire");
+  const vooraf = document.getElementById("voorafgaand");
   const test = document.getElementById("test-interface");
-  const post = document.getElementById("post-questionnaire");
+  const achteraf = document.getElementById("achteraf");
 
-  document.getElementById("verdervoor").addEventListener("click", function () {
-    pre.style.display = "none";
+  const knopVoor = document.getElementById("verdervoor");
+  const knopTest = document.getElementById("verdertest");
+  const knopAchteraf = document.getElementById("verderachteraf");
+
+  knopVoor.addEventListener("click", function () {
+    vooraf.style.display = "none";
     test.style.display = "block";
   });
 
-  document.getElementById("verdertest").addEventListener("click", function () {
+  knopTest.addEventListener("click", function () {
     test.style.display = "none";
-    post.style.display = "block";
+    achteraf.style.display = "block";
   });
 
-  document.getElementById("verderachteraf").addEventListener("click", function () {
-    alert("Bedankt voor het invullen van de test!");
+  knopAchteraf.addEventListener("click", function () {
+    alert("Bedankt voor uw deelname");
   });
 });
