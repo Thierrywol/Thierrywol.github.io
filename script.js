2badd10a7d9841a4f38225a252b453f3e25e6cad
Thierrywol <tdwolffenbuttel1@gmail.com>
2025-06-02 15:04:38 +0200

    kaspers versie

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const pre = document.getElementById("pre-questionnaire");
-  const test = document.getElementById("test-interface");
-  const post = document.getElementById("post-questionnaire");
-
-  document.getElementById("verdervoor").addEventListener("click", function () {
-    pre.style.display = "none";
-    test.style.display = "block";
-  });
-
-  document.getElementById("verdertest").addEventListener("click", function () {
-    test.style.display = "none";
-    post.style.display = "block";
-  });
-
-  document.getElementById("verderachteraf").addEventListener("click", function () {
-    alert("Bedankt voor het invullen van de test!");
-  });
-});
-document.addEventListener("DOMContentLoaded", function () {
   const progressBar = document.getElementById("progress-bar");
 
   function updateProgress(percentage) {
